5e999272a60de4acdaa8e6c6bb1adf66c06c80b3
Gabriel Garcia <80603461+ggarcia453@users.noreply.github.com>
2023-10-14 18:09:02 -0700

    Original Landing Page

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # AroundtheWorldRadio
+
+This a public repository for the Around the World Radio Senior Design Project. This project has been created Isabel Ruiz, Antoine Nguyen, and Gabriel Garcia, and advised by Professor Peter Burke from UCI's Electrical Engineering and Computer Science department. 
--- radio-site/src/App.js
+++ radio-site/src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to the Around the World Radio Project website.
         </p>
         <a
           className="App-link"
@@ -15,8 +14,17 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Psk reporter - original project for reference
         </a>
+        <p>
+        This website has been created by Antoine Nguyen, Isabel Ruiz, and Gabriel Garcia as a part of the Senior Capstone design. This project was advised
+        by Prof. Burke of UCI's Electrical Engineering and Computer Science Department. For more information about this project and the students,
+        please follow
+        <a  className="App-link"
+          href="https://sites.uci.edu/eecscapstone/2023/05/04/around-the-world-radio/"
+          target="_blank"
+          rel="noopener noreferrer"> this link.</a>
+      </p>
       </header>
     </div>
   );
