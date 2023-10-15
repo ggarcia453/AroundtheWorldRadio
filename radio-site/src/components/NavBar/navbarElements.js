8feafc2883c968fafec12dc46d67ed66cfe56814
Gabriel Garcia <80603461+ggarcia453@users.noreply.github.com>
2023-10-14 22:49:33 -0700

    Work on Multiple pages
    We are beginning the first steps for creating mutliple pages. If we are unable to implement this before the map then we will work towards moving the map onto the first page.

---
+++ radio-site/src/components/NavBar/navbarElements.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {
+    Nav,
+    NavLink,
+    Bars,
+    NavMenu,
+    NavBtn,
+    NavBtnLink,
+} from "react-bootstrap/Navbar";
+ 
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <Bars />
+ 
+                    
+            </Nav>
+        </>
+    );
+};
+ 
+export default Navbar;
