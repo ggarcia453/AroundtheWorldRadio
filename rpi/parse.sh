685871f75ab48a04d534cbc4900833ac5458cc0c
danae76 <danae76@hotmail.com>
2023-12-05 23:39:23 +0000

    Changed run.py and parse.sh

--- rpi/parse.sh
+++ rpi/parse.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "HI"
-python3 parse.py all.txt
-rm all.txt
+python3 parse.py ~/.local/share/WSJT-X/ALL.TXT
+rm ~/.local/share/WSJT-X/ALL.TXT
--- rpi/run.py
+++ rpi/run.py
@@ -1,14 +1,16 @@
 import subprocess
 import time, datetime
 import sys
+import os
 
 def main():
     while True:
         print(datetime.datetime.now())
-        subprocess.call('parse.sh')
-        time.sleep(5)
+        subprocess.call(['sh','./parse.sh'])
+        print("Finished...")
+        time.sleep(3600)
 
 if __name__ == "__main__":
-    arguments = sys.argv[1:]
-    if len(arguments) > 0:
+    #arguments = sys.argv[1:]
+    #if len(arguments) > 0:
         main()
