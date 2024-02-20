661e952d2be2401f2c5779e64020251f56bb60ed
Gabriel Garcia <80603461+ggarcia453@users.noreply.github.com>
2024-02-20 15:09:37 -0800

    Manual on website now

--- radio-site/src/pages/Manual.jsx
+++ radio-site/src/pages/Manual.jsx
@@ -1,41 +1,33 @@
-import React from "react";
- 
-const Manual = () => {
-    return (
-        <div
-            style={{
-                justifyContent: "left",
-                alignItems: "left",
-                textAlign:"left"
-            }}
-        >
-            <h1>
-                Manual
-            </h1>
-            <h2>
-                COMPONENTS USED
-            </h2>
-            <p> Here is a list of our hardware and software components.</p>
-            <h3>Hardware Componnets</h3>
-            <ol>
-                <li>Raspberry Pi 4 Model B</li>
-                <li>RTL-SDR Dongle Model: V3</li>
-                <li>Antenna</li>
-            </ol>
-            <h3>Software Components</h3>
-            <h2>ANTENNA</h2>
-            <h2>ANTENNA BUILDING</h2>
-            <h2>MONGODB CONFIGURATION</h2>
-            <h2>WEBSITE BUILDING</h2>
-            <pre><code class="lang-javascript">import React from "react";<br/>
-            import L from 'leaflet'; <br/>
-            import 
-            import "leaflet/dist/leaflet.css";</code></pre>
-            <h2>RASPBERRY PI SETUP</h2>
-            <h2>SIGNAL DATA ROADMAP</h2>
-            <h2>Common Errors</h2>
+import React, { Component } from 'react'
+import ReactMarkdown from 'react-markdown'
+import termsFrPath from './manual.md'
+
+
+class Terms extends Component {
+    constructor(props) {
+      super(props)
+  
+      this.state = { terms: null }
+    }
+  
+    componentWillMount() {
+      fetch(termsFrPath).then((response) => response.text()).then((text) => {
+        this.setState({ terms: text })
+      })
+    }
+  
+    render() {
+      return (
+        <div className="content"
+        style={{
+            justifyContent: "left",
+            alignItems: "left",
+            textAlign:"left"
+        }}>
+          <ReactMarkdown children={this.state.terms} />
         </div>
-    );
-};
- 
-export default Manual;
+      )
+    }
+  }
+  
+  export default Terms;
